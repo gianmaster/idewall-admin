afd9ec0f66de933f4468131de0d5b7ec7c2eae20
GianMaster <giancarloscercado@gmail.com>
2016-10-18 17:36:56 -0500

    save test vuetable

--- resources/assets/js/finalComponents/app/reporte_horarios_cursos/test-plugin.vue
+++ resources/assets/js/finalComponents/app/reporte_horarios_cursos/test-plugin.vue
@@ -3,8 +3,21 @@
     <div class="row">
         <div class="col-xs-12">
             <h2>Esperando por los plugins</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo ratione rerum aliquam, nisi aut neque nam modi id iure obcaecati iste, placeat voluptatum vitae amet cum doloremque quas unde? Molestias.</p>
-            <p>{{msg}}</p>
+            <div id="app" class="ui vertical stripe segment">
+                <div class="ui container">
+                    <div id="content" class="ui basic segment">
+                        <h3 class="ui header">List of Users</h3>
+                        <vuetable
+                            api-url="api/v2/data"
+                            table-wrapper="#content"
+                            data-path="data"
+                            pagination-path=""
+                            :fields="columns"
+                            :item-actions="itemActions"
+                        ></vuetable>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
@@ -17,9 +30,40 @@
     import VuetablePaginationDropdown from 'vuetable/src/components/VuetablePaginationDropdown.vue';
 
     export default{
+        components: {
+            Vuetable, VuetablePagination, VuetablePaginationDropdown
+        },
         data: function(){
             return {
-                msg: 'hola mundo'
+                columns: [
+                    'nombres',
+                    'apellidos',
+                    'email',
+                    'identificacion',
+                    'genero',
+                    '__actions'
+                ],
+                itemActions: [
+                    { name: 'view-item', label: '', icon: 'zoom icon', class: 'ui teal button' },
+                    { name: 'edit-item', label: '', icon: 'edit icon', class: 'ui orange button'},
+                    { name: 'delete-item', label: '', icon: 'delete icon', class: 'ui red button' }
+                ]
+            }
+        },
+        methods: {
+            viewProfile: function(id) {
+                console.log('view profile with id:', id)
+            }
+        },
+        events: {
+            'vuetable:action': function(action, data) {
+                console.log('vuetable:action', action, data)
+                if (action == 'view-item') {
+                    this.viewProfile(data.id)
+                }
+            },
+            'vuetable:load-error': function(response) {
+                console.log('Load Error: ', response)
             }
         }
     }
